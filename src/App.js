478605bcbb82ef462b939c5991426225ae28411d
Atthaset <iaut2545@gmail.com>
2023-02-02 16:27:03 +0700

    Complete2/2/2023

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from './Data/Data.js'
+import { useState } from "react"
+import SingleContent from './Components/SingleContent';
 
 function App() {
+
+  const [content, setContent] = useState(Data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <h3>My favorite cat type</h3>
+        <section>
+          {content.map((e) => {
+            return <SingleContent key={e.id} {...e}/>
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
